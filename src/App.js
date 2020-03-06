ec1e6aef3271a9dc198acfa35f3f0487b5e8645f
unknown <2373638339@qq.com>
2020-03-06 12:50:26 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
+import stats from './067-stats.png';
+import lottery from './130-lottery.png';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/ArrowBack';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+      <Router>
+      <div>
+ 
+ <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" >
+            News
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      
+
+     
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className='item'>
+<img src={lottery} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          老化抽屉预测试
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        </div>
+        <div className='item'>
+<img src={stats} className="App-logo" alt="logo" />
+        <p>
+          老化测试
+        </p>
+       
+        </div>
       </header>
     </div>
+          </Route>
+      <Route  path="/about">
+            <div className="App">
+     
+        <Button variant="contained" color="primary">
+          你好，世界
+        </Button>
+      
+    </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
